9b45ebaecee45519c9acfbb3146b8ceccf951012
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2023-06-24 22:30:50 +0530

    Add files via upload

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-
+const userRoutes = require("./routes/user.route");
+const bookRoutes = require("./routes/book.route");
 
 const app = express();
 
@@ -12,6 +13,13 @@ app.use(
 
 app.use(express.json());
 
+// Route for accessing all books
+app.get("/", (req, res) => {
+  res.send("Access all books /book");
+});
 
+/* Import all routes */
+app.use("/user", userRoutes); 
+app.use("/book", bookRoutes); 
 
 module.exports = app;
